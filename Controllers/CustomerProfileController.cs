4cf414178e71e1762daf48673ca3f578548c5f7b
Troy Krause <troykrause07@gmail.com>
2024-08-16 17:31:26 +0200

    Capture Profile Info UI

---
+++ Controllers/CustomerProfileController.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using ABCRetail.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABCRetail.Controllers
+{
+    public class CustomerProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateProfile()
+        {
+            return View("CustomerProfileCapturePage");
+        }
+
+        [HttpPost]
+        public IActionResult CreateProfile(CustomerProfileRequest request)
+        {
+
+            // Handle file upload and save the profile information to the database
+            var fileName = Path.GetFileName(request.ProfilePhoto.FileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                request.ProfilePhoto.CopyTo(stream);
+            }
+
+
+            return RedirectToAction("ProfileSuccess"); // Redirect to a success page or action
+
+        }
+
+        public IActionResult ProfileSuccess()
+        {
+            return View("CustomerProfileCaptureSuccessPage");
+        }
+    }
+}
---
+++ Models/CustomerProfileRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ABCRetail.Models
+{
+    public class CustomerProfileRequest
+    {
+      
+        
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string LastName { get; set; }
+
+            [Required, EmailAddress]
+            public string Email { get; set; }
+
+            [Required, Phone]
+            public string Phone { get; set; }
+
+            [Required]
+            public string Address { get; set; }
+
+            public IFormFile ProfilePhoto { get; set; }
+        }
+    }
+
+
+
